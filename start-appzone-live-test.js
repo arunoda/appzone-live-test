71540ad39fcb9afbf2d32247ac064ebdf9c046df
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-12-04 10:16:26 +0530

    added start script

---
+++ start-appzone-live-test.js
@@ -0,0 +1,31 @@
+//loading nconf
+var nconf = require('nconf');
+nconf.add('file', { file: './conf/config.json' });
+nconf.load();
+
+//loading winstoon
+var winstoon = require('winstoon');
+winstoon.add(winstoon.transports.Console, {timestamp: true});
+var logger = winstoon.createLogger('start');
+
+//load mongo
+var mongo = require('mongoskin');
+var mongoConfig = nconf.get('mongo');
+
+var appCollection = mongo.db(mongoConfig.url).collection(mongoConfig.collections.app);
+var phoneCollection = mongo.db(mongoConfig.url).collection(mongoConfig.collections.phone);
+
+//load the model
+var Model = require('./lib/model');
+var model = new Model(appCollection, phoneCollection);
+
+//load Routers
+var appzoneConfig = nconf.get('appzone');
+var ports = nconf.get('ports');
+
+var MtRouter = require('./lib/mtRouter');
+var mtRouter = new MtRouter(appzoneConfig, ports.mt, model);
+
+var MoRouter = require('./lib/moRouter');
+var moRouter = new MtRouter(appzoneConfig, ports.mo, model);
+
